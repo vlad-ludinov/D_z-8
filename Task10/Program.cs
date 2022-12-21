95d315b7b0c719163fd16c87d724062ae93b8df1
vlad <vladislavludinov3006@gmail.com>
2022-12-21 20:55:34 +0300

    Task10 complete

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,7 @@
 ﻿using static System.Console;
 
+Write("Введите трёхзначное число: ");
 int number = int.Parse(ReadLine()!);
+
+WriteLine(99<number && number<1000?number%100/10:"число не трёхзначное");
 
